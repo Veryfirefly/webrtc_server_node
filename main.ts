a52a49f44aa789d0d20991a5fbce18dba9d03b6d
GangHuo <993610942@qq.com>
2024-05-10 17:17:50 +0800

    Added typescript main.ts

---
+++ main.ts
@@ -0,0 +1,17 @@
+class Dummy {
+    private address: string;
+    private port: number;
+
+    constructor(address: string, port: number) {
+        this.address = address;
+        this.port = port
+    }
+
+    public start(): void {
+
+    }
+
+    public shutdown(): void {
+        
+    }
+}
