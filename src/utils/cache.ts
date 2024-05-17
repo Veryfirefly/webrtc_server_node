6349af6cb3e56e8ba989ee70d030ff27766e118f
GangHuo <993610942@qq.com>
2024-05-17 23:21:06 +0800

    1. add default expired time

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -2,6 +2,9 @@ import * as NodeCache from 'node-cache';
 
 export const cache = new NodeCache();
 
+// expiration 30day.
+export const defaultExpireTime: number = 30 * 24 * 60 * 60;
+
 export const tokenKey = (key: string): string => {
     return `token:${key}`;
 }
